ae5cd3bf7b93a04c6a96f29f67b54b4383b3d516
croissen214 <croissen214@gmail.com>
2025-08-05 19:02:43 +0900

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // 변경
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import Compare from './pages/Compare';
